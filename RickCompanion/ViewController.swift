12b00413f50c669203d2199c47305fd0079048e0
MuhammedGamal <Muhammed.gamal@vezeeta.com>
2024-09-02 21:37:56 +0300

    creating the project

---
+++ RickCompanion/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RickCompanion
+//
+//  Created by Jimmy on 02/09/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
